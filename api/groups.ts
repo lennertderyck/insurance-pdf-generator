5bced974b8583e6ed427f85cb751b79b2be47413
Lennert De Ryck <lennyderyck@gmail.com>
2025-01-03 22:22:12 +0100

    Update groups.ts

--- api/groups.ts
+++ api/groups.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 import csv from 'csvtojson';
 
-export const config = {
-  runtime: 'nodejs',
-};
-
 export const GET = async (request: Request) => {
   const re = await axios('https://docs.google.com/spreadsheets/d/e/2PACX-1vRIffwV3pGKdE9Cu-sn0HYm6UWKtsSue4HgvfmbpTl73v-eNWXpiEclLjFH8hoj6P6O266A7qJmUp1o/pub?gid=0&single=true&output=csv')
   const converted = await csv().fromString(re.data);
